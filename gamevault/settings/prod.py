9a943a41c069b1bf6c172112fe1849ed092a12a5
Ростислав Дурягін <rostyslav.duriahin@lnu.edu.ua>
2026-08-13 10:52:54 +0200

    Add RENDER_EXTERNAL_HOSTNAME

--- gamevault/settings/prod.py
+++ gamevault/settings/prod.py
@@ -3,10 +3,11 @@ from gamevault.settings.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 SECURE_SSL_REDIRECT = True
 
